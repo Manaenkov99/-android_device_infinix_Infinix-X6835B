b54a1cb86af2878197cb2813244fbfd628e20ccf
Manaenkov99 <753159jj@gmail.com>
2024-06-30 03:15:18 +0300

    1

--- twrp_Infinix-X6835B.mk
+++ twrp_Infinix-X6835B.mk
@@ -9,14 +9,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6835B device
 $(call inherit-product, device/infinix/Infinix-X6835B/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6835B
-PRODUCT_NAME := omni_Infinix-X6835B
+PRODUCT_NAME := twrp_Infinix-X6835B
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6835B
 PRODUCT_MANUFACTURER := infinix
